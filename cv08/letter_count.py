5d6581f897ad3290053b4038d3ebbc2d92313140
Martin Granc3k Simon <mart.simon17@gmail.com>
2024-11-25 15:42:23 +0100

    cv08 done & black formating

--- cv08/letter_count.py
+++ cv08/letter_count.py
@@ -1,3 +1,4 @@
+# made by Martin "Granc3k" Šimon
 import argparse
 import logging
 import sys
@@ -15,24 +16,28 @@ def count_words_by_letter(input_path):
 
     # Define sourcu
     ds = env.from_source(
-        source=FileSource.for_record_stream_format(StreamFormat.text_line_format(),
-                                                   input_path)
-                         .process_static_file_set().build(),
+        source=FileSource.for_record_stream_format(
+            StreamFormat.text_line_format(), input_path
+        )
+        .process_static_file_set()
+        .build(),
         watermark_strategy=WatermarkStrategy.for_monotonous_timestamps(),
-        source_name="file_source"
+        source_name="file_source",
     )
 
     # Process kazdeho radku
     def extract_words(line):
         # Split na slova, normalizace do lowercase, a filtr podle A-Z
-        words = re.findall(r'\b[a-zA-Z]+\b', line.lower())
-        return [(word[0], 1) for word in words if 'a' <= word[0] <= 'z']
+        words = re.findall(r"\b[a-zA-Z]+\b", line.lower())
+        return [(word[0], 1) for word in words if "a" <= word[0] <= "z"]
 
     # Process dat
     letter_counts = (
-        ds.flat_map(extract_words, output_type=Types.TUPLE([Types.STRING(), Types.INT()]))
-          .key_by(lambda i: i[0])  # Group by the first letter
-          .reduce(lambda i, j: (i[0], i[1] + j[1]))  # Sum counts
+        ds.flat_map(
+            extract_words, output_type=Types.TUPLE([Types.STRING(), Types.INT()])
+        )
+        .key_by(lambda i: i[0])  # Group by the first letter
+        .reduce(lambda i, j: (i[0], i[1] + j[1]))  # Sum counts
     )
 
     # Print výsledků
@@ -42,15 +47,16 @@ def count_words_by_letter(input_path):
     env.execute()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--input',
-        dest='input',
+        "--input",
+        dest="input",
         required=True,
-        help='Input directory containing text files to process.')
+        help="Input directory containing text files to process.",
+    )
 
     argv = sys.argv[1:]
     known_args, _ = parser.parse_known_args(argv)
--- cv08/letter_count_web_logging.py
+++ cv08/letter_count_web_logging.py
@@ -4,32 +4,30 @@ import logging
 import sys
 import re
 
-from pyflink.common import Types, Duration
-from pyflink.common.watermark_strategy import WatermarkStrategy
-from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.common import WatermarkStrategy, Types
+from pyflink.datastream import StreamExecutionEnvironment, RuntimeExecutionMode
 from pyflink.datastream.connectors import FileSource, StreamFormat
 
 
-def count_words_by_letter_streaming(input_path):
+def count_words_by_letter(input_path):
     env = StreamExecutionEnvironment.get_execution_environment()
-    env.set_parallelism(1)
+    env.set_runtime_mode(RuntimeExecutionMode.BATCH)
+    env.set_parallelism(1)  # Setnutní paralelismu na 1
 
-    # Define sourcu + checkovani novych souboru
+    # Define sourců
     ds = env.from_source(
         source=FileSource.for_record_stream_format(
             StreamFormat.text_line_format(), input_path
         )
-        .monitor_continuously(
-            Duration.of_seconds(1)
-        )  # Check noveho filu kazdou 1 sekundu
+        .process_static_file_set()
         .build(),
-        watermark_strategy=WatermarkStrategy.for_monotonous_timestamps(),  # Dummy watermark strategy
+        watermark_strategy=WatermarkStrategy.for_monotonous_timestamps(),
         source_name="file_source",
     )
 
-    # Process kazdeho radku
+    # Process řádků
     def extract_words(line):
-        # Split na slova, normalizace do lowercase, a filtr podle A-Z
+        # Split na slova, normalizace na lowercase, a filtr podle A-Z
         words = re.findall(r"\b[a-zA-Z]+\b", line.lower())
         return [(word[0], 1) for word in words if "a" <= word[0] <= "z"]
 
@@ -38,18 +36,24 @@ def count_words_by_letter_streaming(input_path):
         ds.flat_map(
             extract_words, output_type=Types.TUPLE([Types.STRING(), Types.INT()])
         )
-        .key_by(lambda i: i[0])  # Group by podle first letter
-        .reduce(lambda i, j: (i[0], i[1] + j[1]))  # Sum counts
+        .key_by(lambda i: i[0])  # Group by podle 1. písmene
+        .reduce(lambda i, j: (i[0], i[1] + j[1]))  # Sum počtů
     )
 
-    # Print vysledku
-    letter_counts.print()
+    # Loging výsledků
+    def log_results(letter_count):
+        letter, count = letter_count
+        logging.info(f"{letter}: {count}")
 
-    # Executene environment
-    env.execute()
+    # Přidání logování výsledků
+    letter_counts.map(lambda x: log_results(x))
+
+    # Start prostředí
+    env.execute("Count Words by Letter")
 
 
 if __name__ == "__main__":
+    # Conf logování
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
 
     parser = argparse.ArgumentParser()
@@ -57,10 +61,10 @@ if __name__ == "__main__":
         "--input",
         dest="input",
         required=True,
-        help="Input directory to monitor for new files.",
+        help="Input directory containing text files to process.",
     )
 
     argv = sys.argv[1:]
     known_args, _ = parser.parse_known_args(argv)
 
-    count_words_by_letter_streaming(known_args.input)
+    count_words_by_letter(known_args.input)
